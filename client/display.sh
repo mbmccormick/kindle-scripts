a1cf8955194a10e2631c551aa606e59d81178887
Matt McCormick <matt@mbmccormick.com>
2013-04-29 09:26:08 -0700

    Adjusting client script to download new image first, before clearing the screen.

--- client/display.sh
+++ client/display.sh
@@ -3,11 +3,13 @@
 cd "$(dirname "$0")"
 
 rm display.png
-eips -c
-eips -c
 
 if wget -O display.png http://matt-kindle.webscript.io/index; then
+	eips -c
+	eips -c
 	eips -g display.png
 else
+	eips -c
+	eips -c
 	eips -g error.png
 fi
